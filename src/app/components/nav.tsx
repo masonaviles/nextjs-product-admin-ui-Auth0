0f4d9eea289f61d239a3da0483bfce4d18c7573d
masonaviles <mce.aviles@gmail.com>
2024-03-26 20:29:14 -0700

    start basic nav

--- src/app/components/nav.tsx
+++ src/app/components/nav.tsx
@@ -1,26 +1,18 @@
+"use client"
+
 import React from 'react';
 import Link from 'next/link';
 
 const Nav = () => {
+
   return (
-    <nav className="bg-gray-800 text-white h-full">
-      <div className="px-5 py-4">
-        <div className="font-bold text-xl mb-4">Opal</div>
-        <ul className="space-y-3">
-          <li>
-            <Link href="/" className="hover:text-gray-300 transition-colors duration-200">Dashboard</Link>
-          </li>
-          <li>
-            <Link href="/about" className="hover:text-gray-300 transition-colors duration-200">Contracts</Link>
-          </li>
-          <li>
-            <Link href="/services" className="hover:text-gray-300 transition-colors duration-200">Account</Link>
-          </li>
-          <li>
-            <Link href="/contact" className="hover:text-gray-300 transition-colors duration-200">Contact</Link>
-          </li>
-        </ul>
-      </div>
+    <nav className="bg-gray-800 text-white h-full flex flex-col">
+      <h1 className="header-title">Opal</h1>
+      <Link href="/" className="hover:text-gray-300 transition-colors duration-200">My Dashboard</Link>
+      <Link href="/contracts" className="hover:text-gray-300 transition-colors duration-200">My Contracts</Link>
+      <Link href="/generator" className="hover:text-gray-300 transition-colors duration-200">Generator</Link>
+      <Link href="/account" className="hover:text-gray-300 transition-colors duration-200">Account</Link>
+      <Link href="/contact" className="hover:text-gray-300 transition-colors duration-200">Contact</Link>
     </nav>
   );
 };
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { ReactNode } from 'react';
 import Nav from './components/Nav'; // Adjust the path as needed
 import "./styles/globals.css";
+import "./styles/styles.css";
 
 // Assuming "Inter" font and "metadata" are used elsewhere or in a custom _document.tsx or _app.tsx
 // const inter = Inter({ subsets: ["latin"] });
@@ -16,9 +17,13 @@ type RootLayoutProps = {
 
 export default function RootLayout({ children }: RootLayoutProps) {
   return (
-    <div>
-      <Nav /> {/* Include the Nav component at the top */}
-      <main>{children}</main> {/* Main content area for child components/pages */}
+    <div className="grid grid-cols-[1fr_6fr] min-h-screen">
+      <div className="bg-gray-800 py-9 pl-9"> {/* Assuming you want to keep the nav background */}
+        <Nav />
+      </div>
+      <main className="bg-white">
+        {children}
+      </main>
     </div>
   );
 }
