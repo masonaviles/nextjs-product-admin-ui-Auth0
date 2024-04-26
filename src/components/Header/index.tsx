520543dbe7338f731d001c807e3dd2f1e2e570ab
masonaviles <mce.aviles@gmail.com>
2024-04-26 07:36:26 -0700

    remove icon from mobile nav

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -11,7 +11,7 @@ const Header = (props: {
 }) => {
   return (
     <header className="sticky top-0 z-999 flex w-full bg-white drop-shadow-1 dark:bg-boxdark dark:drop-shadow-none">
-      <div className="flex flex-grow items-center justify-end px-4 py-4 shadow-2 md:px-6 2xl:px-11">
+      <div className="flex flex-grow items-center justify-end px-4 py-4 shadow-2 md:px-6 2xl:px-11 gap-2">
         <div className="flex items-center gap-2 sm:gap-4 lg:hidden">
           {/* <!-- Hamburger Toggle BTN --> */}
           <button
@@ -55,15 +55,6 @@ const Header = (props: {
             </span>
           </button>
           {/* <!-- Hamburger Toggle BTN --> */}
-
-          <Link className="block flex-shrink-0 lg:hidden" href="/">
-            <Image
-              width={32}
-              height={32}
-              src={"/images/logo/opal-icon.jpg"}
-              alt="Logo"
-            />
-          </Link>
         </div>
 
         {/* <div className="hidden sm:block">
